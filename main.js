953b8c641f5593130760c39189df4ebea7eb58ef
ibrahims168 <ibrahh881@gmail.com>
2022-03-01 17:07:44 +0200

    adding backend3

--- main.js
+++ main.js
@@ -3,14 +3,24 @@ const title = document.getElementById('title')
 const userNameInput = document.getElementById('userNameInput')
 const passwordInput = document.getElementById('passwordInput')
 const signInBtn = document.getElementById('signInBtn')
+const mainContainer = document.getElementById('mainContainer')
+
+const renderUser = function (user) {
+    const firstName = document.createElement('h2')
+    firstName.innerText = `first name: ${user.firstName}`
+    const lastName = document.createElement('h2')
+    firstName.innerText = `last name: ${user.lastName}`
+    mainContainer.append(firstName,lastName)
+}
+
 
 const submit = function () {
     const userName = userNameInput.value
     const password = passwordInput.value
-
     const user = school.signIn(userName,password)
+    
 }
-
+    signInBtn.addEventListener('click',submit)
 
 // -------------------------------------------
 
